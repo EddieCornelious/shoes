7ca018dc729945fd1ed6813309602c82e9989e81
EddieCornelious <eulerissmart@gmail.com>
2019-12-01 13:53:59 -0500

    css fixes footer

--- src/components/Social.js
+++ src/components/Social.js
@@ -11,49 +11,59 @@ const Social = () => {
     <React.Fragment>
       <div className="container-fluid no__padding">
         <div className="row">
-          <div className="social__wrap">
+          <div
+            style={{ backgroundImage: `url(${twitter})` }}
+            className="social__wrap"
+          >
             <div className="social__content">
               <i class="fab fa-twitter" />
               twitter
             </div>
             <div className="social__overlay twitter--overlay" />
-            <img className="social" src={twitter} />
           </div>
 
-          <div className="social__wrap">
+          <div
+            style={{ backgroundImage: `url(${fb})` }}
+            className="social__wrap"
+          >
             <div className="social__content">
               <i className="fab fa-facebook-f" />
               facebook
             </div>
             <div className="social__overlay facebook--overlay" />
-            <img className="social" src={fb} />
           </div>
 
-          <div className="social__wrap">
+          <div
+            style={{ backgroundImage: `url(${insta})` }}
+            className="social__wrap"
+          >
             <div className="social__content">
               <i class="fab fa-instagram" />
               instagram
             </div>
             <div className="social__overlay instagram--overlay" />
-            <img className="social" src={insta} />
           </div>
 
-          <div className="social__wrap">
+          <div
+            style={{ backgroundImage: `url(${yt})` }}
+            className="social__wrap"
+          >
             <div className="social__content">
               <i class="fab fa-youtube" />
               youtube
             </div>
             <div className="social__overlay youtube--overlay" />
-            <img className="social" src={yt} />
           </div>
 
-          <div className="social__wrap">
+          <div
+            style={{ backgroundImage: `url(${map})` }}
+            className="social__wrap"
+          >
             <div className="social__content">
               <i class="far fa-map" />
               find us
             </div>
             <div className="social__overlay map--overlay" />
-            <img className="social" src={map} />
           </div>
         </div>
       </div>
