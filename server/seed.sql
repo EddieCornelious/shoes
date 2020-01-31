6230ec0aeba280181c20cc7d8e25125738891325
EddieCornelious <eulerissmart@gmail.com>
2020-01-31 11:31:31 -0500

    added data to tables

--- server/seed.sql
+++ server/seed.sql
@@ -1,344 +1,94 @@
-USE shoe_store;
+use shoe_store;
 
 /* product types*/
-INSERT INTO product_types
-            (NAME)
-VALUES     ("bicycle");
-
-INSERT INTO product_types
-            (NAME)
-VALUES     ("snowboard");
+INSERT INTO  Product_Types(name) 
+VALUES("bicycle"); 
+INSERT INTO  Product_Types(name) 
+VALUES("snowboard"); 
 
 /*
  colors
 */
-INSERT INTO colors
-            (NAME)
-VALUES     ("red");
-
-INSERT INTO colors
-            (NAME)
-VALUES     ("white");
-
-INSERT INTO colors
-            (NAME)
-VALUES     ("black");
-
-INSERT INTO colors
-            (NAME)
-VALUES     ("blue");
-
-INSERT INTO colors
-            (NAME)
-VALUES     ("orange");
-
-INSERT INTO colors
-            (NAME)
-VALUES     ("yellow");
+INSERT INTO  colors(name) 
+VALUES("red"); 
+INSERT INTO  colors(name) 
+VALUES("white"); 
+INSERT INTO  colors(name) 
+VALUES("black"); 
+INSERT INTO  colors(name) 
+VALUES("blue"); 
+INSERT INTO  colors(name) 
+VALUES("orange"); 
+INSERT INTO  colors(name) 
+VALUES("yellow"); 
 
 /* bikes*/
-INSERT INTO products
-            (price,
-             product_type,
-             image_url,
-             NAME)
-VALUES     (289,
-            1,
-            "/images/unknown-1.png",
-            "pro road bike");
-
-INSERT INTO products
-            (price,
-             product_type,
-             image_url,
-             NAME)
-VALUES     (400,
-            1,
-            "/images/unknown-2.jpeg",
-            "prestige road bike");
-
-INSERT INTO products
-            (price,
-             product_type,
-             image_url,
-             NAME)
-VALUES     (289,
-            1,
-            "/images/unknown-3.jpeg",
-            "darmanatan cruiser");
-
-INSERT INTO products
-            (price,
-             product_type,
-             image_url,
-             NAME)
-VALUES     (500,
-            1,
-            "/images/unknown-4.jpeg",
-            "korno road bike" );
-
-INSERT INTO products
-            (price,
-             product_type,
-             image_url,
-             NAME)
-VALUES     (289,
-            1,
-            "/images/unknown-5.jpeg",
-            "splinter road bike");
-
-INSERT INTO products
-            (price,
-             product_type,
-             image_url,
-             NAME)
-VALUES     (489,
-            1,
-            "/images/unknown-6.jpeg",
-            "paolo road bike");
-
-INSERT INTO products
-            (price,
-             product_type,
-             image_url,
-             NAME)
-VALUES     (300,
-            1,
-            "/images/unknown-7.jpeg",
-            "porto road bike");
-
-INSERT INTO products
-            (price,
-             product_type,
-             image_url,
-             NAME)
-VALUES     (700,
-            1,
-            "/images/unknown-8.jpeg",
-            "excel road bike");
-
-INSERT INTO products
-            (price,
-             product_type,
-             image_url,
-             NAME)
-VALUES     (755,
-            1,
-            "/images/unknown-9.jpeg",
-            "pro road cruiser");
-
-INSERT INTO products
-            (price,
-             product_type,
-             image_url,
-             NAME)
-VALUES     (689,
-            1,
-            "/images/unknown-10.jpeg",
-            "porto road cruiser");
-
-INSERT INTO products
-            (price,
-             product_type,
-             image_url,
-             NAME)
-VALUES     (289,
-            1,
-            "/images/unknown-11.jpeg",
-            "korno road cruiser");
-
-INSERT INTO products
-            (price,
-             product_type,
-             image_url,
-             NAME)
-VALUES     (289,
-            1,
-            "/images/unknown-12.jpeg",
-            "prestige road cruiser");
-
-INSERT INTO products
-            (price,
-             product_type,
-             image_url,
-             NAME)
-VALUES     (275,
-            1,
-            "/images/unknown-13.jpeg",
-            "diplomat road bike");
-
-INSERT INTO products
-            (price,
-             product_type,
-             image_url,
-             NAME)
-VALUES     (700,
-            1,
-            "/images/unknown-14.jpeg",
-            "exchange road bike");
-
-INSERT INTO products
-            (price,
-             product_type,
-             image_url,
-             NAME)
-VALUES     (700,
-            1,
-            "/images/unknown-15.jpeg",
-            "falmo road cruiser");
-
-INSERT INTO products
-            (price,
-             product_type,
-             image_url,
-             NAME)
-VALUES     (900,
-            1,
-            "/images/unknown-16.jpeg",
-            "givendales road bike");
-
-INSERT INTO products
-            (price,
-             product_type,
-             image_url,
-             NAME)
-VALUES     (500,
-            1,
-            "/images/unknown-17.jpeg",
-            "givendales road cruiser");
-
-INSERT INTO products
-            (price,
-             product_type,
-             image_url,
-             NAME)
-VALUES     (567,
-            1,
-            "/images/unknown-18.jpeg",
-            "borker road bike");
-
-INSERT INTO products
-            (price,
-             product_type,
-             image_url,
-             NAME)
-VALUES     (285,
-            1,
-            "/images/unknown-19.jpeg",
-            "borker road cruiser");
+ INSERT INTO  Products(Price, Product_Type, Image_Url, Name) 
+VALUES(289, 1, "/images/Unknown-1.png", "Pro Road Bike"); 
+INSERT INTO  Products(Price, Product_Type, Image_Url,Name) 
+VALUES(400, 1, "/images/Unknown-2.jpeg", "Prestige Road Bike"); 
+INSERT INTO  Products(Price, Product_Type, Image_Url,Name) 
+VALUES(289, 1, "/images/Unknown-3.jpeg", "Darmanatan Cruiser"); 
+INSERT INTO  Products(Price, Product_Type, Image_Url,Name) 
+VALUES(500, 1, "/images/Unknown-4.jpeg", "Korno Road Bike" ); 
+INSERT INTO  Products(Price, Product_Type, Image_Url,Name) 
+VALUES(289, 1, "/images/Unknown-5.jpeg", "Splinter Road Bike"); 
+INSERT INTO  Products(Price, Product_Type, Image_Url,Name) 
+VALUES(489, 1, "/images/Unknown-6.jpeg", "Paolo Road Bike"); 
+INSERT INTO  Products(Price, Product_Type, Image_Url,Name) 
+VALUES(300, 1, "/images/Unknown-7.jpeg", "Porto Road Bike"); 
+INSERT INTO  Products(Price, Product_Type, Image_Url,Name) 
+VALUES(700, 1, "/images/Unknown-8.jpeg", "Excel Road Bike"); 
+INSERT INTO  Products(Price, Product_Type, Image_Url,Name) 
+VALUES(755, 1, "/images/Unknown-9.jpeg", "Pro Road Cruiser"); 
+INSERT INTO  Products(Price, Product_Type, Image_Url,Name) 
+VALUES(689, 1, "/images/Unknown-10.jpeg", "Porto Road Cruiser"); 
+INSERT INTO  Products(Price, Product_Type, Image_Url,Name) 
+VALUES(289, 1, "/images/Unknown-11.jpeg", "Korno Road Cruiser"); 
+INSERT INTO  Products(Price, Product_Type, Image_Url,Name) 
+VALUES(289, 1, "/images/Unknown-12.jpeg", "Prestige Road Cruiser"); 
+INSERT INTO  Products(Price, Product_Type, Image_Url,Name) 
+VALUES(275, 1, "/images/Unknown-13.jpeg", "Diplomat Road Bike"); 
+INSERT INTO  Products(Price, Product_Type, Image_Url,Name) 
+VALUES(700, 1, "/images/Unknown-14.jpeg", "Exchange Road Bike"); 
+INSERT INTO  Products(Price, Product_Type, Image_Url,Name) 
+VALUES(700, 1, "/images/Unknown-15.jpeg", "Falmo Road Cruiser"); 
+INSERT INTO  Products(Price, Product_Type, Image_Url,Name) 
+VALUES(900, 1, "/images/Unknown-16.jpeg", "Givendales Road Bike"); 
+INSERT INTO  Products(Price, Product_Type, Image_Url,Name) 
+VALUES(500, 1, "/images/Unknown-17.jpeg", "Givendales Road Cruiser"); 
+INSERT INTO  Products(Price, Product_Type, Image_Url,Name) 
+VALUES(567, 1, "/images/Unknown-18.jpeg", "Borker Road Bike"); 
+INSERT INTO  Products(Price, Product_Type, Image_Url,Name) 
+VALUES(285, 1, "/images/Unknown-19.jpeg", "Borker Road Cruiser");
 
 /* boards*/
-INSERT INTO products
-            (price,
-             product_type,
-             image_url,
-             NAME)
-VALUES     (100,
-            2,
-            "/images/untitled-1129_copy_copy_large.jpeg",
-            "borkar snowboard");
+INSERT INTO  Products(Price, Product_Type, Image_Url,Name) 
+VALUES(100, 2, "/images/Untitled-1129_copy_copy_large.jpeg", "Borkar Snowboard");
+INSERT INTO  Products(Price, Product_Type, Image_Url,Name) 
+VALUES(400, 2, "/images/Untitled-112_copy_copy_large.jpeg", "Borkar Ravage Snowboard");
+INSERT INTO  Products(Price, Product_Type, Image_Url,Name) 
+VALUES(200, 2, "/images/Untitled-112_copy_copy_copy_large.jpeg", "Borkar Shear Snowboard");
+INSERT INTO  Products(Price, Product_Type, Image_Url,Name) 
+VALUES(500, 2, "/images/Untitled-12_copy_copy_84666978-c3c0-4651-8ab9-d0b6086a57c2_large.jpeg", "Borkar Potrum Snowboard");
+INSERT INTO  Products(Price, Product_Type, Image_Url,Name) 
+VALUES(500, 2, "/images/capita-volcom-stone-snowboard_large.jpeg", "Blaze Snowboard");
+INSERT INTO  Products(Price, Product_Type, Image_Url,Name) 
+VALUES(400, 2, "/images/capita-indoor-survival-snowboard-156-top_large_f7aeafff-b798-4e62-9061-5f599c597ab8_large.jpeg", "Komodo Dragon Snowboard");
+INSERT INTO  Products(Price, Product_Type, Image_Url,Name) 
+VALUES(300, 2, "/images/capita-indoor-survival-snowboard-156-top_large.jpeg", "Komodo Leopard Snowboard");
+INSERT INTO  Products(Price, Product_Type, Image_Url,Name) 
+VALUES(700, 2, "/images/capita-defenders-of-awesome-snowboard-154-top-red-base_large_copy_d7c8ea4b-06cb-49bd-b9f6-6a37c356fb01_large.jpeg", "Komodo Dream Snowboard");
+INSERT INTO  Products(Price, Product_Type, Image_Url,Name) 
+VALUES(600, 2, "/images/ADYSB00020_MUL_FRT6_1800-2400_large.jpeg", "Komodo Sensor Snowboard");
+INSERT INTO  Products(Price, Product_Type, Image_Url,Name) 
+VALUES(500, 2, "/images/ADYSB00020_MUL_FRT4_1800-2400_large.jpeg", "Komodo Dive Snowboard");
+INSERT INTO  Products(Price, Product_Type, Image_Url,Name) 
+VALUES(600, 2, "/images/ADYSB00019_MUL_FRT4_1800-2400_large.jpeg", "Komodo Drift Snowboard");
 
-INSERT INTO products
-            (price,
-             product_type,
-             image_url,
-             NAME)
-VALUES     (400,
-            2,
-            "/images/untitled-112_copy_copy_large.jpeg",
-            "borkar ravage snowboard");
 
-INSERT INTO products
-            (price,
-             product_type,
-             image_url,
-             NAME)
-VALUES     (200,
-            2,
-            "/images/untitled-112_copy_copy_copy_large.jpeg",
-            "borkar shear snowboard");
 
-INSERT INTO products
-            (price,
-             product_type,
-             image_url,
-             NAME)
-VALUES     (500,
-            2,
-"/images/untitled-12_copy_copy_84666978-c3c0-4651-8ab9-d0b6086a57c2_large.jpeg",
-"borkar potrum snowboard");
 
-INSERT INTO products
-            (price,
-             product_type,
-             image_url,
-             NAME)
-VALUES     (500,
-            2,
-            "/images/capita-volcom-stone-snowboard_large.jpeg",
-            "blaze snowboard");
+show tables;
 
-INSERT INTO products
-            (price,
-             product_type,
-             image_url,
-             NAME)
-VALUES     (400,
-            2,
-"/images/capita-indoor-survival-snowboard-156-top_large_f7aeafff-b798-4e62-9061-5f599c597ab8_large.jpeg"
-            ,
-"komodo dragon snowboard");
 
-INSERT INTO products
-            (price,
-             product_type,
-             image_url,
-             NAME)
-VALUES     (300,
-            2,
-            "/images/capita-indoor-survival-snowboard-156-top_large.jpeg",
-            "komodo leopard snowboard");
-
-INSERT INTO products
-            (price,
-             product_type,
-             image_url,
-             NAME)
-VALUES     (700,
-            2,
-"/images/capita-defenders-of-awesome-snowboard-154-top-red-base_large_copy_d7c8ea4b-06cb-49bd-b9f6-6a37c356fb01_large.jpeg"
-            ,
-"komodo dream snowboard");
-
-INSERT INTO products
-            (price,
-             product_type,
-             image_url,
-             NAME)
-VALUES     (600,
-            2,
-            "/images/adysb00020_mul_frt6_1800-2400_large.jpeg",
-            "komodo sensor snowboard");
-
-INSERT INTO products
-            (price,
-             product_type,
-             image_url,
-             NAME)
-VALUES     (500,
-            2,
-            "/images/adysb00020_mul_frt4_1800-2400_large.jpeg",
-            "komodo dive snowboard");
-
-INSERT INTO products
-            (price,
-             product_type,
-             image_url,
-             NAME)
-VALUES     (600,
-            2,
-            "/images/adysb00019_mul_frt4_1800-2400_large.jpeg",
-            "komodo drift snowboard");
-
-SELECT *
-FROM   products; 
--- server/tables.sql
+++ server/tables.sql
@@ -14,9 +14,11 @@ DROP TABLE users;
 
 CREATE TABLE IF NOT EXISTS users
   (
-     user_id  INT auto_increment PRIMARY KEY NOT NULL,
-     username VARCHAR(255) NOT NULL,
-     pword    VARCHAR(255) NOT NULL
+     user_id    INT auto_increment PRIMARY KEY NOT NULL,
+     username   VARCHAR(255) NOT NULL UNIQUE,
+     pword      VARCHAR(255) NOT NULL,
+     email      VARCHAR(255) UNIQUE NOT NULL,
+     created_at DATETIME DEFAULT CURRENT_TIMESTAMP
   );
 
 CREATE TABLE IF NOT EXISTS product_types
@@ -29,8 +31,9 @@ CREATE TABLE IF NOT EXISTS products
   (
      product_id   INT auto_increment PRIMARY KEY NOT NULL,
      price        INTEGER NOT NULL,
-     product_type INTEGER NOT NULL,
+     product_type INTEGER NOT NULL, 
      name         VARCHAR(255) UNIQUE NOT NULL,
+     created_at   DATETIME DEFAULT CURRENT_TIMESTAMP,
      image_url    VARCHAR(1000) NOT NULL,
      FOREIGN KEY (product_type) REFERENCES product_types(producttype_id)
   );
@@ -41,8 +44,17 @@ CREATE TABLE IF NOT EXISTS orders
      product_id INTEGER NOT NULL,
      fulfilled  BOOL NOT NULL DEFAULT false,
      user_id    INTEGER NOT NULL,
+     created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
      FOREIGN KEY (product_id) REFERENCES products(product_id),
      FOREIGN KEY (user_id) REFERENCES users(user_id)
+  );
+
+CREATE TABLE IF NOT EXISTS cart_items
+  (
+     belongs_to INTEGER NOT NULL,
+     product_id INTEGER NOT NULL,
+     FOREIGN KEY (product_id) REFERENCES products(product_id),
+     FOREIGN KEY (belongs_to) REFERENCES users(user_id)
   );
 
 CREATE TABLE IF NOT EXISTS colors
@@ -50,6 +62,3 @@ CREATE TABLE IF NOT EXISTS colors
      color_id INTEGER auto_increment NOT NULL PRIMARY KEY,
      name     VARCHAR(50) NOT NULL UNIQUE
   );
-
-SELECT *
-FROM   products;
