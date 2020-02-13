8fad959c2c574782f54a81902244828d6255de00
EddieCornelious <eulerissmart@gmail.com>
2020-02-12 19:48:27 -0500

    added hash and salt function

--- server/api/users.js
+++ server/api/users.js
@@ -1,16 +1,15 @@
 const crypto = require("crypto");
 const { fork } = require("child_process");
 
-const process = fork("./genpw.js");
+const process = fork("../helpers/HashSalt.js");
 
 //signup
-//process.send({username: "ed", pw: "lol", type: 1});
+//process.send({ username: "ed", pw: "lol", type: 1 });
 
 //login
-//process.send({username: "ed", pw: "lol", type: 0, salt: "salt"});
+process.send({ username: "ed", pw: "lol", type: 0, salt: "salt" });
 
 process.on("message", message => {
   console.log(message.hashSalt);
   console.log(message.salt);
-  process.kill();
 });
--- server/helpers/HashSalt.js
+++ server/helpers/HashSalt.js
@@ -19,6 +19,7 @@ const run = (function() {
         if (err) throw err;
 
         process.send({ hashSalt: derivedKey.toString("hex"), salt });
+        process.exit(0);
       }
     );
   });
