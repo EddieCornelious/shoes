e49cbd1fc1a4fdffe50eadb439cd712b5117ab56
EddieCornelious <eulerissmart@gmail.com>
2020-02-21 15:11:46 -0500

    added fake liek functionality

--- src/components/Home/TrendingSection.js
+++ src/components/Home/TrendingSection.js
@@ -1,41 +1,52 @@
 import React, { Component } from "react";
 import "../../styles/TrendingSection.scss";
 
-const TrendingItem = ({ classId }) => {
-  const url = [
-    "Unknown-2.jpeg",
-    "Unknown-3.jpeg",
-    "Unknown.png",
-    "Unknown-1.jpeg",
-    "Unknown-4.jpeg",
-    "Unknown-8.jpeg",
-    "Unknown-18.jpeg",
-    "Unknown-19.jpeg"
-  ];
-  return (
-    <div className="col-xs-12 col-sm-6 col-md-6 col-lg-3 p-0">
-      <div className="trending__item">
-        <div className="trending__item__top">
-          <img src={"images/" + url[classId]} />
-        </div>
-        <div className="trending__item__bottom">
-          <div className="trending__overlay">
-            <a href="#" className="trending__cart">
-              ADD TO CART
-            </a>
+const url = [
+  "Unknown-2.jpeg",
+  "Unknown-3.jpeg",
+  "Unknown.png",
+  "Unknown-1.jpeg",
+  "Unknown-4.jpeg",
+  "Unknown-8.jpeg",
+  "Unknown-18.jpeg",
+  "Unknown-19.jpeg"
+];
+
+class TrendingItem extends React.Component {
+  state = { liked: false };
+
+  render() {
+    return (
+      <div className="col-xs-12 col-sm-6 col-md-6 col-lg-3 p-0">
+        <div className="trending__item">
+          <div className="trending__item__top">
+            <img src={"images/" + url[this.props.classId]} />
           </div>
-          <h3>Really Cool Bike</h3>
-          <p>$499</p>
-          <i className="fa fa-star" aria-hidden="true" />
-          <i className="fa fa-star" aria-hidden="true" />
-          <i className="fa fa-star" aria-hidden="true" />
-          <i className="fa fa-star" aria-hidden="true" />
-          <i className="fa fa-star" aria-hidden="true" />
+          <div className="trending__item__bottom">
+            <div className="trending__overlay">
+              <a href="#" className="trending__cart">
+                ADD TO CART
+              </a>
+              <i
+                className={
+                  this.state.liked ? "fas fa-heart liked" : "far fa-heart"
+                }
+                onClick={() => this.setState({ liked: !this.state.liked })}
+              />
+            </div>
+            <h3>Really Cool Bike</h3>
+            <p>$499</p>
+            <i className="fa fa-star" aria-hidden="true" />
+            <i className="fa fa-star" aria-hidden="true" />
+            <i className="fa fa-star" aria-hidden="true" />
+            <i className="fa fa-star" aria-hidden="true" />
+            <i className="fa fa-star" aria-hidden="true" />
+          </div>
         </div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
 class TrendingSection extends React.Component {
   componentDidMount() {}
